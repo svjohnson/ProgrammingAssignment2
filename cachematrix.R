b29c5581de852e0abb43416223d913b3172b3974
svjohnson <samjohnson05@hotmail.com>
2014-10-26 18:34:37 -0400

    committed cachematrix.R from svjohnson. For programming assignment 2 for R_programming for user svjohnson

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 
+## This is a function that can create a matrix object, which can also cache its inverse, presuming 
+## the matrix is square and invertible. 
+## 
 
-## Write a short comment describing this function
+## this is for R_programming Assigment 2 lexical scoping; to be used with cacheSolve.
+#1.     set the value of the matrix
+#2.     get the value of the matrix
+#3.        set the value of the matrix's inverse
+#1.        get the value of the matrix's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve) 
+        
 }
 
 
-## Write a short comment describing this function
+## This is a function that can create will cache the inverse of a matrix object, 
+## presuming the matrix is square and invertible. 
+## 
+
+## this is for R_programming Assigment 2 lexical scoping; to be used with makeCacheMatrix.R
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting matrix inverse from cache")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
+        
 }
